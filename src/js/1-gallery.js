c0b24bfecb1cbf31dc844b3e436e5ec3b25a163b
Kostiantyn2020 <161701157+Kostiantyn2020@users.noreply.github.com>
2024-07-11 22:39:51 +0300

    3

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,9 @@
-console.log(Galerry);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+
+console.log(SimpleLightbox);
